3768555d134baf745fd28cc3e9cedc75392a491e
Jacob Phillips <jakephillips.wv@gmail.com>
2022-09-22 13:50:11 -0500

    initial commit

---
+++ BankEncapsulation/Program.cs
@@ -0,0 +1,10 @@
+﻿using BankEncapsulation;
+
+var account1 = new BankAccount();
+
+Console.Write("Please enter deposit amount: ");
+var userDepositAmount = double.Parse(Console.ReadLine());
+
+account1.Deposit(userDepositAmount);
+
+Console.WriteLine($"New Balance: {account1.GetBalance()}");
