6bbe5df46028c0ac94d15e01d0b086b9d40c929b
Jacob Phillips <jakephillips.wv@gmail.com>
2022-09-22 21:20:51 -0500

    updated to replace Parse with TryParse

--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -3,7 +3,15 @@
 var account1 = new BankAccount();
 
 Console.Write("Please enter deposit amount: $");
-var userDepositAmount = double.Parse(Console.ReadLine());
+
+double userDepositAmount;
+var userDepositValid = double.TryParse(Console.ReadLine(), out userDepositAmount);
+
+while (!userDepositValid)
+{
+    Console.Write("Invalid entry. Please enter valid deposit amount: $");
+    userDepositValid = double.TryParse(Console.ReadLine(), out userDepositAmount);
+}
 
 account1.Deposit(userDepositAmount);
 
