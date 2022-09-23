49996404a51b82ef4eb5002021c885eabb334726
Jacob Phillips <jakephillips.wv@gmail.com>
2022-09-23 14:11:23 -0500

    improved functionality with loops

--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -2,33 +2,56 @@
 
 var account1 = new BankAccount();
 
-Console.Write("Please enter deposit amount: $");
-
-double userDepositAmount;
-var userDepositValid = double.TryParse(Console.ReadLine(), out userDepositAmount);
-
-while (!userDepositValid)
+string userEntry = "1";
+while (userEntry == "1")
 {
-    Console.Write("Invalid entry. Please enter valid deposit amount: $");
-    userDepositValid = double.TryParse(Console.ReadLine(), out userDepositAmount);
+    Console.WriteLine("Would you like to make a Deposit or Withdrawl?");
+    Console.Write("(Enter 1 for Deposit or 2 for Withdrawl) ");
+    userEntry = Console.ReadLine();
+    Console.WriteLine();
+
+    if (userEntry == "1")
+    {                     
+        Console.Write("Please enter deposit amount: $");
+
+        var userDepositValid = double.TryParse(Console.ReadLine(), out double userDepositAmount);
+        Console.WriteLine();
+
+        while (!userDepositValid)
+        {
+            Console.Write("Invalid entry. Please enter valid deposit amount: $");
+            userDepositValid = double.TryParse(Console.ReadLine(), out userDepositAmount);
+            Console.WriteLine();
+        }
+
+        account1.Deposit(userDepositAmount);
+
+        Console.WriteLine($"New Balance: ${account1.GetBalance()}");
+        Console.WriteLine();        
+    }
+    if (userEntry == "2")
+    {
+        Console.Write("Please enter withdrawl amount: $");
+
+        var userWithrawlValid = double.TryParse(Console.ReadLine(), out double userWithrawlAmount);
+        Console.WriteLine();
+
+        while (!userWithrawlValid)
+        {
+            Console.Write("Invalid entry. Please enter valid withdrawl amount: $");
+            userWithrawlValid = double.TryParse(Console.ReadLine(), out userWithrawlAmount);
+            Console.WriteLine();
+        }
+
+        account1.Withdrawl(userWithrawlAmount);
+
+        Console.WriteLine($"New Balance: ${account1.GetBalance()}");
+        Console.WriteLine();
+                
+        userEntry = "1";
+    }
 }
 
-account1.Deposit(userDepositAmount);
 
-Console.WriteLine($"New Balance: ${account1.GetBalance()}");
-Console.WriteLine();
 
-Console.Write("Please enter withdrawl amount: $");
 
-double userWithrawlAmount;
-var userWithrawlValid = double.TryParse(Console.ReadLine(), out userWithrawlAmount);
-
-while (!userWithrawlValid)
-{
-    Console.Write("Invalid entry. Please enter valid withdrawl amount: $");
-    userWithrawlValid = double.TryParse(Console.ReadLine(), out userWithrawlAmount);
-}
-
-account1.Withdrawl(userWithrawlAmount);
-
-Console.WriteLine($"New Balance: ${account1.GetBalance()}");
